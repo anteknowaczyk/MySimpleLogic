04749d7d4e1d60f0a720fa178763f435f33c0e18
Antoni Nowaczyk <antoninowaczyk1@gmail.com>
2024-10-15 20:23:48 +0200

    first commit

---
+++ src/main/java/com/mycompany/mysimplelogic/MySimpleLogic.java
@@ -0,0 +1,12 @@
+package com.mycompany.mysimplelogic;
+
+/**
+ *
+ * @author Antoni Jan Nowaczyk
+ */
+public class MySimpleLogic {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
